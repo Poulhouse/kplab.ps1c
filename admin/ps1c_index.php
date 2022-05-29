5bb2a75d418fd72c456795c5343094efb65dbbdf
Pavel Karpenko <karpenko.p.s@gmail.com>
2022-05-30 02:17:52 +0300

    create `admin/ps1c_index.php`

---
+++ admin/ps1c_index.php
@@ -0,0 +1,12 @@
+<?php
+define('ADMIN_MODULE_NAME', 'kplab.ps1c');
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_admin_before.php';
+
+// @todo: Здесь - какой-то системный код, читающие данные и всё такое
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_admin_after.php';
+
+echo "Welcome to admin area";
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/epilog_admin.php';
